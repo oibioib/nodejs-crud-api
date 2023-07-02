6c90cd7addb7356c2d8b5b1c215dbd337b939de0
oibioib <pavelbodal@gmail.com>
2023-07-02 14:00:56 +0200

    feat: add errors description

---
+++ src/config/errors.ts
@@ -0,0 +1,14 @@
+export const ERRORS = {
+  NO_ENDPOINT: {
+    code: 404,
+    message: 'Oops! Endpoint does not exist!',
+  },
+  INVALID_USER_ID: {
+    code: 400,
+    message: '{userId} is invalid (not uuid)!',
+  },
+  USER_NOT_EXIST: {
+    code: 404,
+    message: 'User does not exist!',
+  },
+};
